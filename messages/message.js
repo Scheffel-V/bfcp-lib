1d8b80094666b1de0d38a165b8f3d579200e1732
Scheffel <brsufirefox@gmail.com>
2019-04-02 12:11:27 -0300

    Message model have now a name field, representing it Primitive. Like: 'FloorRequest', 'Hello', 'FloorStatus'...

--- messages/message.js
+++ messages/message.js
@@ -2,10 +2,12 @@ class Message {
   constructor(commonHeader, attributes) {
     this._commonHeader = commonHeader;
     this._attributes = attributes;
+    this._name = commonHeader ? this._primitiveName(commonHeader) : null;
   }
 
   set commonHeader(commonHeader) {
     this._commonHeader = commonHeader;
+    this.name = this._primitiveName(commonHeader);
   }
 
   get commonHeader() {
@@ -18,6 +20,47 @@ class Message {
 
   get attributes() {
     return this._attributes;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  _primitiveName(commonHeader) {
+    switch(commonHeader.primitive) {
+      case 1:
+        return 'FloorRequest';
+      case 2:
+        return 'FloorRelease';
+      case 3:
+        return 'FloorRequestQuery';
+      case 4:
+        return 'FloorRequestStatus';
+      case 5:
+        return 'UserQuery';
+      case 6:
+        return 'UserStatus';
+      case 7:
+        return 'FloorQuery';
+      case 8:
+        return 'FloorStatus';
+      case 9:
+        return 'ChairAction';
+      case 10:
+        return 'ChairActionAck';
+      case 11:
+        return 'Hello';
+      case 12:
+        return 'HelloAck';
+      case 13:
+        return 'Error';
+      default:
+        throw new Error("Error in message primitive!");
+    }
   }
 
   encode() {
