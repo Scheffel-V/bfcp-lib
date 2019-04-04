3488ab1dba99d537240eec95d5ab218ab3e85b37
Scheffel <brsufirefox@gmail.com>
2019-04-04 15:02:26 -0300

    - User model:   * FloorRequestId is now static   * some messages receive a previous message as argument   * UserId and conferenceId are now converted from string to integer - User realocated to a new file - Client file deleted, since doesn't need it anymore - Config file deleted, since doesn't need it anymore

--- messages/message.js
+++ messages/message.js
@@ -2,12 +2,10 @@ class Message {
   constructor(commonHeader, attributes) {
     this._commonHeader = commonHeader;
     this._attributes = attributes;
-    this._name = commonHeader ? this._primitiveName(commonHeader) : null;
   }
 
   set commonHeader(commonHeader) {
     this._commonHeader = commonHeader;
-    this.name = this._primitiveName(commonHeader);
   }
 
   get commonHeader() {
@@ -28,43 +26,6 @@ class Message {
 
   set name(name) {
     this._name = name;
-  }
-
-  _primitiveName(commonHeader) {
-    switch(commonHeader.primitive) {
-      case 1:
-        return 'FloorRequest';
-      case 2:
-        return 'FloorRelease';
-      case 3:
-        return 'FloorRequestQuery';
-      case 4:
-        return 'FloorRequestStatus';
-      case 5:
-        return 'UserQuery';
-      case 6:
-        return 'UserStatus';
-      case 7:
-        return 'FloorQuery';
-      case 8:
-        return 'FloorStatus';
-      case 9:
-        return 'ChairAction';
-      case 10:
-        return 'ChairActionAck';
-      case 11:
-        return 'Hello';
-      case 12:
-        return 'HelloAck';
-      case 13:
-        return 'Error';
-      case 14:
-        return 'FloorRequestStatusAck';
-      case 16:
-        return 'FloorStatusAck';
-      default:
-        throw new Error("Error in message primitive!");
-    }
   }
 
   encode() {
---
+++ user/user.js
@@ -0,0 +1,102 @@
+const Parser = require('../parser/parser.js');
+const Primitive = require('../messages/primitive.js');
+const RequestStatusValue = require('../messages/requestStatusValue.js');
+const HelloAck = require('../messages/helloAck.js');
+const FloorRequestStatus = require('../messages/floorRequestStatus.js');
+const FloorStatus = require('../messages/floorStatus.js');
+
+class User {
+  constructor(userId, conferenceId) {
+    this._userId = parseInt(userId);
+    this._conferenceId = parseInt(conferenceId);
+    this._currentMessage = null;
+    this._currentTransactionId = 0;
+    this._floorRequestId = 0;
+  }
+
+  set userId(userId) {
+    this._userId = userId;
+  }
+
+  get userId() {
+    return this._userId;
+  }
+
+  set conferenceId(conferenceId) {
+    this._conferenceId = conferenceId;
+  }
+
+  get conferenceId() {
+    return this._conferenceId;
+  }
+
+  set currentMessage(currentMessage) {
+    this._currentMessage = currentMessage;
+  }
+
+  get currentMessage() {
+    return this._currentMessage;
+  }
+
+  set currentTransactionId(currentTransactionId) {
+    this._currentTransactionId = currentTransactionId;
+  }
+
+  get currentTransactionId() {
+    return this._currentTransactionId;
+  }
+
+  static getFloorRequestId() {
+    return this.FloorRequestId;
+  }
+
+  static incFloorRequestId() {
+    this.FloorRequestId++;
+  }
+
+  set wantedFloorId(wantedFloorId) {
+    this._wantedFloorId = wantedFloorId;
+  }
+
+  get wantedFloorId() {
+    return this._wantedFloorId;
+  }
+
+  receiveMessage(message) {
+    try {
+      let bfcpMessage = Parser.parseMessage(message);
+      this.currentMessage = bfcpMessage;
+      return bfcpMessage;
+    } catch(error) {
+      throw error;
+    }
+  }
+
+  helloAckMessage(helloMessage) {
+    let helloAck = new HelloAck(this.conferenceId, helloMessage.commonHeader.transactionId, this.userId);
+    return Buffer.from(helloAck.encode());
+  }
+
+  floorRequestStatusMessage(message, floorId, requestStatus) {
+    let floorRequestId;
+    if(requestStatus == RequestStatusValue.Granted) {
+      User.incFloorRequestId();
+      floorRequestId = User.getFloorRequestId();
+    } else {
+      floorRequestId = message.attributes[0].content;
+    }
+    let floorRequestStatus = new FloorRequestStatus(this.conferenceId, message.commonHeader.transactionId, this.userId, floorRequestId, floorId, requestStatus);
+    return Buffer.from(floorRequestStatus.encode());
+  }
+
+  floorStatusMessage(floorId, requestStatus) {
+    if(this.currentMessage.commonHeader.transactionId > this.currentTransactionId) {
+      this.currentTransactionId = this.currentMessage.commonHeader.transactionId;
+    }
+    this.currentTransactionId++;
+    let floorStatus = new FloorStatus(this.conferenceId, this.currentTransactionId, this.userId, User.getFloorRequestId(), floorId, requestStatus);
+    return Buffer.from(floorStatus.encode());
+  }
+}
+User.FloorRequestId = 0;
+module.exports = User;
